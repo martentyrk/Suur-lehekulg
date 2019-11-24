044d1ea305ee9b0edd3a5ba1479be1f0bafeda91
Marten <martentyrk@gmail.com>
2019-11-24 11:38:37 +0200

    Added pictures and a little js

--- cursor.js
+++ cursor.js
@@ -1,5 +1,6 @@
 const cursor = document.querySelector(".cursor");
 
 document.addEventListener("mousemove", e => {
-    cursor.setAttribute("style","top: "+e.pageY)
+    console.log(e)
+    //cursor.setAttribute("style","top: "+e.pageY)
 })
